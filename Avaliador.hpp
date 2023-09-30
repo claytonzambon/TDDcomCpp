dc87c117a15cf1f9778b40266619b23842777e0c
Clayton Zambon <zambon@outlook.com.br>
2023-09-30 15:15:00 -0300

    FEAT: Maiores Lances e ordenação

--- Avaliador.hpp
+++ Avaliador.hpp
@@ -6,10 +6,12 @@ class Avaliador
 private:
 	float maiorValor = INT_MIN;
 	float menorValor = INT_MAX;
-
+	std::vector<Lance> maiores3Lances;
+	static bool ordenaLances(const Lance&, const Lance&);
 public:
 	void avalia(Leilao);
 	float recuperaMaiorValor() const;
 	float recuperaMenorValor() const;
+	std::vector<Lance> recupera3MaioresLances() const;
 };
 
