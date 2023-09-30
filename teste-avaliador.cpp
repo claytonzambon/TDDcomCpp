ffefde93a60c9a43bfe08e2087fe4661ceba57b5
Clayton Zambon <zambon@outlook.com.br>
2023-09-29 21:48:49 -0300

    FEAT: Primeito Test Case - Parte 02

--- teste-avaliador.cpp
+++ teste-avaliador.cpp
@@ -24,3 +24,24 @@ TEST_CASE("Deve recuperar maior lance de leilão em ordem Crescente") {
 	// Assert or Then (Então) -> Verificando se tudo deu certo
 	REQUIRE(2000 == leiloeiro.recuperaMaiorValor());
 }
+
+TEST_CASE("Deve recuperar maior lance de leilão em ordem Decrescente") {
+
+	// ArrangeActAssert (Triple A)
+	// GivenWhenThen
+
+	// Arrange or Given (Dado) -> Preparando ambiente para os testes
+	Lance primeiroLance(Usuario("Clayton Zambon"), 2000);
+	Lance segundoLance(Usuario("Karine Zambon"), 1000);
+	Leilao leilao("Fiat 147 0Km");
+	leilao.recebeLance(segundoLance);
+	leilao.recebeLance(primeiroLance);
+
+	Avaliador leiloeiro;
+
+	// Act or When (Quando) -> Executando o código a ser testado
+	leiloeiro.avalia(leilao);
+
+	// Assert or Then (Então) -> Verificando se tudo deu certo
+	REQUIRE(2000 == leiloeiro.recuperaMaiorValor());
+}
