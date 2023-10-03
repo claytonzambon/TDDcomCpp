9840082800abaae339e4aa4a06dff8fa7d65f776
Clayton Zambon <zambon@outlook.com.br>
2023-10-02 23:31:11 -0300

    TEST: Testando 3 maiores lances - Parte 01

--- teste-avaliador.cpp
+++ teste-avaliador.cpp
@@ -87,3 +87,30 @@ TEST_CASE("Deve recuperar menor lance de leilão em ordem Crescente") {
 	// Assert or Then (Então) -> Verificando se tudo deu certo
 	REQUIRE(1000 == leiloeiro.recuperaMenorValor());
 }
+
+TEST_CASE("Deve recuperar os 3 maiores lances") {
+
+	// ArrangeActAssert (Triple A)
+	// GivenWhenThen
+
+	// Arrange or Given (Dado) -> Preparando ambiente para os testes
+	Lance primeiroLance(Usuario("Clayton Zambon"), 1000);
+	Lance segundoLance(Usuario("Karine Zambon"), 2000);
+	Lance terceiroLance(Usuario("Bernadete"), 1500);
+	Lance quartoLance(Usuario("Miguel"), 2500);
+
+	Leilao leilao("Fiat 147 0Km");
+	leilao.recebeLance(primeiroLance);
+	leilao.recebeLance(segundoLance);
+	leilao.recebeLance(terceiroLance);
+	leilao.recebeLance(quartoLance);
+
+	Avaliador leiloeiro;
+
+	// Act or When (Quando) -> Executando o código a ser testado
+	leiloeiro.avalia(leilao);
+
+	// Assert or Then (Então) -> Verificando se tudo deu certo
+	std::vector<Lance> maiores3Lances = leiloeiro.recupera3MaioresLances();
+	REQUIRE(3 == maiores3Lances.size());
+}
