e2cb62ed4eb4199c0b2abc738b65af454d42faff
Clayton Zambon <zambon@outlook.com.br>
2023-10-04 02:16:22 -0300

    TEST: Lances repetidos - Parte 01

--- teste-avaliador.cpp
+++ teste-avaliador.cpp
@@ -86,3 +86,21 @@ TEST_CASE("Avaliador")
 	}
 }
 
+TEST_CASE("Leilão não deve receber lances consecutivos do mesmo usuário")
+{
+	//Arrange
+	Leilao leilao("Fiat 147 0Km");
+	Usuario zambon("Clayton Zambon");
+
+	Lance primeiroLance(zambon, 1000);
+	Lance segundoLance(zambon, 1500);
+
+	//Act
+	leilao.recebeLance(primeiroLance);
+	leilao.recebeLance(segundoLance);
+
+	//Assert
+	REQUIRE(1 == leilao.recuperaLances().size());
+	REQUIRE(1000 == leilao.recuperaLances()[0].recuperaValor());
+}
+
