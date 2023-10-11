787c404bdfed1f7e62e13141f09653a24870bf37
Clayton Zambon <zambon@outlook.com.br>
2023-10-10 23:20:21 -0300

    FEAT: Small String

--- main.cpp
+++ main.cpp
@@ -3,12 +3,16 @@
 
 void* operator new(size_t bytes)
 {
-	std::cout << "Alocando" << bytes << " bytes" << std::endl;
+	std::cout << "Alocando " << bytes << " bytes" << std::endl;
 	return malloc(bytes);
 }
 
 int main()
 {
 	std::cout << "----------------------" << std::endl;
+	std::string meuNomeCompleto = "Clayton Zambon";
+
+	std::cout << meuNomeCompleto << std::endl;
+	
 	return 0;
 }
